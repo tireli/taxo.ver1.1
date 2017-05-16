beeed88f07cdac3e12a6053c6a0e44d43deea077
aleksei <p2007pab@yandex.ru>
2017-05-17 01:33:36 +0300

    Работа с системой счетов
    TODO
    Методы записи истории
    Создание отчета на основе счетов
    Работа с резидентностью

--- src/Model/Account.java
+++ src/Model/Account.java
@@ -8,13 +8,17 @@ public class Account extends Model implements Serializable {
 	private double curentAccount; // Driver account
 	private double curentDebtIn;	// We owe the money to the driver	
 	private double curentDebtOut;	//The driver owed money to us
-	private double correctionofbill; //correction of bill	
+	private double correctionOfBill; //correction of bill	
 	private double curentAccrual;	//Accrual
 	private double curentPay;		// Pay
 	private ArrayList<accountHistoryItem>  accountHistory;
 	private int accountID;
 	
 	private static int SET_ACCRUAL = 1;
+	private static int SET_PAY = 2;
+	private static int SET_DEBT_OUT = 3;
+	private static int SET_DEBT_IN = 4;
+	
 	
 	
 	public void setAccrual(double newAccrual){
@@ -31,6 +35,127 @@ public class Account extends Model implements Serializable {
 		/*
 		 * make business logic
 		 */
+		/*
+		 * 1 Выполнить списание со счета, 
+		 * если результат отрицательный то обновить дол водителя
+		 * если положительный то обнулить счте и обновить долг
+		 */
+		
+		if (this.curentAccount - this.curentPay >=0) {
+			/*
+			 * деньги есть - выплачиваем
+			 */
+			this.curentAccount -= this.curentPay;
+			this.curentDebtIn = 0;
+			this.curentDebtOut = 0;
+			
+			
+			
+		}else {
+			/*
+			 * денег нету 
+			 */
+			this.curentDebtIn = 0;
+			this.curentDebtOut = this.curentPay - this.curentAccount;
+			this.curentAccount = 0;
+		}
+		/*
+		 * 2 создать запись отчета для формирования ведомости
+		 */
+		//TODO methods
+		
+		
+		/*
+		 * 3 записать историюизменений
+		 */
+		
+		accountHistoryItem item = new accountHistoryItem(SET_PAY, curentAccount, curentDebtIn, curentDebtOut, curentPay);
+		accountHistory.add(item);
+	}
+	
+	public void setPayCancel() {
+		int targetIndexOf = 0;
+		/*
+		 * Отменяет списание по счету, переносит начисление в долг перед водителем
+		 */
+		/*
+		 * 1 получаем последний элемент истории
+		 */
+		accountHistoryItem lastPayHistoryItem = getLastHistoryItem(SET_PAY);
+		
+		/*
+		 * 2 создаем дамп истории изменений счета после полученного элемента
+		 */
+		ArrayList<accountHistoryItem>  dumpAccountHistory = new ArrayList<accountHistoryItem>();
+		try {
+			targetIndexOf = accountHistory.indexOf(lastPayHistoryItem);
+			for (int i = targetIndexOf + 1; i < accountHistory.size(); i++) {
+				dumpAccountHistory.add(accountHistory.get(i));
+			}
+			
+		} catch (NullPointerException e) {
+			// TODO: handle exception
+		}catch (IndexOutOfBoundsException e) {
+			// TODO: handle exception
+		}
+		
+		/*
+		 * 3 изменяем состояние счета в точке истории счета, в которой было произведено списание
+		 */
+		this.curentAccount = lastPayHistoryItem.getCurrentPay();
+		this.curentDebtIn = lastPayHistoryItem.getCurentDebtIn();
+		this.curentDebtOut = lastPayHistoryItem.getCurentDebtOut();
+		this.curentPay = lastPayHistoryItem.getCurentPay();
+		
+		/*
+		 * 4 повторяем историю операций после изменения состояния счета
+		 */
+		
+		try {
+			for (int i = targetIndexOf + 1; i < dumpAccountHistory.size(); i++) {
+				/*
+				 * make switch
+				 * проделать функции по изменению состояния счета согласно истории 
+				 */
+			}
+		} catch (NullPointerException e) {
+			// TODO: handle exception
+		}
+		
+	}
+	
+	public void setDebtOut(double newCurentDebtOut) {
+		this.curentDebtOut = newCurentDebtOut;
+		
+		accountHistoryItem item = new accountHistoryItem(SET_DEBT_OUT, curentDebtOut);
+		accountHistory.add(item);
+	}
+	
+	public void setDebtIn(double newCurentDebtIn) {
+		this.curentDebtIn = newCurentDebtIn;
+		
+		accountHistoryItem item = new accountHistoryItem(SET_DEBT_IN, curentDebtIn);
+		accountHistory.add(item);
+	}
+	
+	
+	
+	
+	
+	private accountHistoryItem getLastHistoryItem(int typeHistoryItem) {
+		// TODO Auto-generated method stub
+		
+		try {
+			for (int i = accountHistory.size(); i < 0; i--) {
+				accountHistoryItem historiItem = accountHistory.get(i);
+				if (historiItem.getType() == typeHistoryItem) {
+					return historiItem;
+				}
+			}
+		} catch (NullPointerException e) {
+			// TODO: handle exception
+		}
+		return null;
 	}
 	
 }
--- src/Model/accountHistoryItem.java
+++ src/Model/accountHistoryItem.java
@@ -10,4 +10,38 @@ public class accountHistoryItem extends Model implements Serializable {
 		
 	}
 
+	public accountHistoryItem(int sET_PAY, double curentAccount,
+			double curentDebtIn, double curentDebtOut, double curentPay) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public accountHistoryItem(int sET_DEBT_OUT, double curentDebtOut) {
+		// TODO Auto-generated constructor stub 
+	}
+
+	public double getCurrentPay() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double getCurentDebtIn() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double getCurentDebtOut() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double getCurentPay() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getType() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
